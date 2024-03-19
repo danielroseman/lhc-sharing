e5121b6a7d66aeb894186897eed89d9d25c15872
Daniel Roseman <daniel.roseman@shopify.com>
2024-03-19 14:13:52 +0000

    WIP: mailchimp

--- music/apps.py
+++ music/apps.py
@@ -1,6 +1,26 @@
 from django.apps import AppConfig
+from django.conf import settings
+from django.dispatch import receiver
+
+from allauth.account.signals import user_signed_up
+from mailchimp_marketing import Client
 
 
 class MusicConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'music'
+
+
+@receiver(user_signed_up)
+def user_signup_callback(request, user):
+    mailchimp = Client()
+    mailchimp.set_config({
+      "api_key": settings.MAILCHIMP_API_KEY,
+      "server": settings.MAILCHIMP_SERVER_PREFIX
+    })
+    mailchimp.lists.add_list_member(
+        settings.MAILCHIMP_LIST_ID,
+        {"email_address": user.email, "status": "unsubscribed", "merge_fields": 
+            {'FNAME': user.first_name, 'LNAME': user.last_name}
+        }
+    )
