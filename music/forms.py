a4d31f8b33d27ee862190d14864ea7dbee96ac60
Daniel Roseman <daniel.roseman@shopify.com>
2024-08-26 13:28:33 +0100

    Add custom signup form with first/last name.
    Improve wording on page.

---
+++ music/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class SignupForm(forms.Form):
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+
+    def signup(self, request, user):
+        pass
