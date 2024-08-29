e03469e3956745657888e060b3caa8383046e040
Daniel Roseman <daniel.roseman@shopify.com>
2024-08-29 20:13:04 +0100

    Remove commented code.

--- lhc_sharing/settings.py
+++ lhc_sharing/settings.py
@@ -48,8 +48,6 @@ elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
     payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
 
     env.read_env(io.StringIO(payload))
-#  else:
-    #  raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
 
 SECRET_KEY = env("SECRET_KEY")
 
