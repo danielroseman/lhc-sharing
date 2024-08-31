f5dc342f505f33bbc2f03e92d4b7d2e5614459d1
Daniel Roseman <daniel.roseman@shopify.com>
2024-08-31 23:24:52 +0100

    Remove Cloud Run settings.

--- lhc_sharing/settings.py
+++ lhc_sharing/settings.py
@@ -19,7 +19,6 @@ from urllib.parse import urlparse
 import environ
 import google.auth
 from google.oauth2 import service_account
-from google.cloud import secretmanager
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -36,14 +35,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 DEBUG = env("DEBUG")
 
-CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
-if CLOUDRUN_SERVICE_URL:
-    ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
-    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
-    SECURE_SSL_REDIRECT = True
-    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
-else:
-    ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
