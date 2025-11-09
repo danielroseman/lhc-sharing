d49da4064befd8028f9987535120b5e6ea71c66f
Daniel Roseman <daniel.roseman@shopify.com>
2025-11-09 16:43:38 +0000

    Add whitenoise.

--- lhc_sharing/settings.py
+++ lhc_sharing/settings.py
@@ -65,6 +65,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "allauth.account.middleware.AccountMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "lhc_sharing.urls"
